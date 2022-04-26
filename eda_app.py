b68fef7b117d27aa55f46d856d5eb0e8018130d6
kstallworth2012 <kennethstallworth@hotmail.com>
2022-04-26 19:56:21 -0400

    Update eda_app.py

--- eda_app.py
+++ eda_app.py
@@ -58,9 +58,9 @@ def run_eda_app():
 
         with st.expander("pledged counts"):
             st.dataframe(df['pledged'].value_counts())
-                fig = plt.figure()
-                sns.countplot(df['pledged'])
-                st.pyplot(fig)
+            fig = plt.figure()
+            sns.countplot(df['pledged'])
+            st.pyplot(fig)
 
         with st.expander("funded percentage counts"):
             st.dataframe(df['funded percentage'].value_counts())
