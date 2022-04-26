4d83770412939b2add6ed7f34e9a17c11f08cfd7
kstallworth2012 <kennethstallworth@hotmail.com>
2022-04-26 14:06:14 -0400

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -1,1 +1,23 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from PIL import Image
+import plotly.express as px
+
+
+#importing ml packages
+import joblib
+import os 
+
+
+
+
+def run_ml_app():
+
+
+    st.subheader("Machine Learning Section")
+    st.write("Looks like its working")
+    st.success("WOW OH WOW ITS SO COOL")
+
