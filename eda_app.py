7a056ad6357401ba1470908d221b8bf556c8b767
kstallworth2012 <kennethstallworth@hotmail.com>
2022-05-04 08:35:15 -0400

    Update eda_app.py

--- eda_app.py
+++ eda_app.py
@@ -30,8 +30,8 @@ def run_eda_app():
     submenu = st.sidebar.selectbox("Submenu",s_menu)
     another_submenu = st.sidebar.selectbox('Another Menu',categorical_cols)
     if submenu == "Descriptive":
-	with st.expander("By Location"):
-		st.dataframe(df['location'].value_counts())
+# 	with st.expander("By Location"):
+# 		st.dataframe(df['location'].value_counts())
 #             local_menu = df['location'].unique().tolist()
 #             mylocalBox=st.selectbox('Location box',local_menu)
 #             loc_df = df[df['location'] ==mylocalBox]
